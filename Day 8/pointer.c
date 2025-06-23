d41230ff7e15d1be787c8261a3e428b3bd9675b7
Shasmitha26 <167598414+Shasmitha26@users.noreply.github.com>
2025-06-23 09:32:13 +0530

    Create pointer.c

---
+++ Day 8/pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void val(int x)
+{
+    x++;
+}
+void ref(int *x)
+{
+    (*x)++;
+}
+void main()
+{
+    /*int a = 10;
+     int *ptr = &a;
+     printf("%d\n", ptr);
+     printf("%d\n", *ptr);
+     printf("%d\n", &a);
+     printf("%p\n", &a);
+     printf("%p\n", ptr);
+    int a=10;
+    val(a);
+    printf("%d\n",a);
+    ref(&a);
+    printf("%d\n", a);*/
+}
